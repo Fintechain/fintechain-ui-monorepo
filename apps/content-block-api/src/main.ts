78c51642508dda0e4a88ea10398cfcfbaec7044c
Jessemeta <jesse@metacubes.co>
2025-02-27 07:06:57 -0800

    stablecoin first commit

--- apps/content-block-api/src/main.ts
+++
@@ -1,9 +0,0 @@
-import {PlatformExpress} from '@tsed/platform-express';
-import {Server} from './server';
-
-async function bootstrap() {
-  const platform = await PlatformExpress.bootstrap(Server);
-  await platform.listen();
-}
-
-bootstrap();
